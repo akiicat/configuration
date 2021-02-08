49c32d87a0862d287558a03f27d59c31d4479d19
Akiicat <aaaa1379@gmail.com>
2021-02-09 00:06:51 +0800

    simplify script.sh

--- script.sh
+++ script.sh
@@ -3,41 +3,15 @@
 set -x
 
 WORKSPACE_PATH=${WORKSPACE_PATH:-/tmp}
-WORKSPACE=${WORKSPACE:-vim}
+WORKSPACE_DIR=${WORKSPACE_DIR:-vim}
 
 cd $WORKSPACE_PATH || { echo "$WORKSPACE_PATH No such file or directory" 1>&2; exit 1; }
-mkdir -p $WORKSPACE || { echo "$PWD/$WORKSPACE already exists but is not a directory" 1>&2; exit 1; }
+mkdir -p $WORKSPACE_DIR || { echo "$PWD/$WORKSPACE_DIR already exists but is not a directory" 1>&2; exit 1; }
 
-cd $WORKSPACE
-
-# wget will overwrite the exist file
-wget https://www.busybox.net/downloads/binaries/1.21.1/busybox-x86_64 -o ./busybox || { echo "download failed" 1>&2; exit 1; }
-chmod u+x ./busybox
-
-[[ -f "busybox" ]] || { echo "busybox not exist"; exit 1; }
-
-wget https://github.com/akiicat/vim/archive/master.zip -o ./vim.zip || { echo "download failed" 1>&2; exit 1; }
-./busybox unzip vim.zip -d .
-
-
-while [[ $# -gt 0 ]];
-do
-    opt="$1";
-    shift;
-    case "$opt" in
-        "all" ) all=1 ;;
-        "tmux" ) cd $WORKSPACE_PATH/$WORKSPACE/tmux && ./install ;;
-        "bash" ) cd $WORKSPACE_PATH/$WORKSPACE/bash && ./install ;;
-        "vim" )  cd $WORKSPACE_PATH/$WORKSPACE/vim  && ./install ;;
-        "git" )  cd $WORKSPACE_PATH/$WORKSPACE/git  && ./install ;;
-        "docker" ) docker=1 ;;
-        *) echo >&2 "Invalid option: $opt"; exit 1;;
-    esac
-done
-
+cd $WORKSPACE_DIR
 
 url=https://github.com/akiicat/vim.git
-path=/tmp/akiicat/vim
+path=$WORKSPACE_PATH/$WORKSPACE_DIR/vim
 if [ -d "$path" ]; then
   cd $path
   git pull;
@@ -46,30 +20,17 @@ else
   cd $path
 fi
 
-if [[ $all == "1" ]] || [[ $bash == "1" ]];
-then
-        echo "Install bash"
-        cd "$path/bash" && ./install
-fi
-if [[ $all == "1" ]] || [[ $tmux == "1" ]];
-then
-        echo "Install tmux"
-        cd "$path/tmux" && ./install
-fi
-if [[ $all == "1" ]] || [[ $git == "1" ]];
-then
-        echo "Install git"
-        cd "$path/git" && ./install
-fi
-if [[ $all == "1" ]] || [[ $vim == "1" ]];
-then
-        echo "Install vim"
-        cd "$path/vim" && ./install
-fi
-if [[ $all == "1" ]] || [[ $docker == "1" ]];
-then
-        echo "Install docker"
-        cd "$path/docker" && ./install
-fi
+while [[ $# -gt 0 ]]; do
+    opt="$1";
+    shift;
+    case "$opt" in
+        "tmux" )    cd $WORKSPACE_PATH/$WORKSPACE_DIR/vim/tmux    && ./install ;;
+        "bash" )    cd $WORKSPACE_PATH/$WORKSPACE_DIR/vim/bash    && ./install ;;
+        "vim" )     cd $WORKSPACE_PATH/$WORKSPACE_DIR/vim/vim     && ./install ;;
+        "git" )     cd $WORKSPACE_PATH/$WORKSPACE_DIR/vim/git     && ./install ;;
+        "docker" )  cd $WORKSPACE_PATH/$WORKSPACE_DIR/vim/docker  && ./install ;;
+        *) echo >&2 "Invalid option: $opt"; exit 1;;
+    esac
+done
 
 set +x
